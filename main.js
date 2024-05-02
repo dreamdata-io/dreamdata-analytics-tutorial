7d482f36d343677db86617ff64a88f4bf9d8ac97
Aleksandar Grbic <hi@aleksandar.xyz>
2024-05-02 14:37:07 +0200

    feat: add gtm

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Analytics!</h1>
-    <div class="card">
-      <button id="counter" type="button">Track</button>
-    </div>
-  </div>
-`
-
+import "./style.css";
